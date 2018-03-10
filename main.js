049fe740b6da5a949c64a04627d94505f46af1f5
eGathergood <ewan_good@hotmail.co.uk>
2018-03-10 22:18:49 +0000

    Extra info added to pop-ups

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //set map nad intial coordinates
-var mymap = L.map('mapid').setView([0, 0], 1);
+var mymap = L.map('mapid').setView([1,1], 1);
 
 var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'});
 Esri_WorldImagery.addTo(mymap);
@@ -70,7 +70,7 @@ $('#dropey').click(function(){
              fillOpacity: 0.8
            })
 
-           circle.addTo(mymap).bindPopup(''+shower.name+'');
+           circle.addTo(mymap).bindPopup("Name: " + ''+shower.name+'' + "<br> Year: " + ''+shower.year+'');
             //add to map
             circle.addTo(mymap);
         }

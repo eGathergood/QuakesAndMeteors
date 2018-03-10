518d4649d5a6d6542ef0e1db5bf9f1762d3a8d29
eGathergood <ewan_good@hotmail.co.uk>
2018-03-10 17:32:39 +0000

    Json grab

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ var mymap = L.map('mapid').setView([0, 0], 1);
 
 var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'});
 Esri_WorldImagery.addTo(mymap);
+
+//shakey on-click
+
+$('#shakey').click(function(){
+
+  console.log("getting quakes");
+
+  $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson", function(result) {
+
+    console.log(result)
+
+  });
+
+});
